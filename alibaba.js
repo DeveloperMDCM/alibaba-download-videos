201705c0b11a5f6dbf071d751c74238e6044a2e6
MDCM <54366580+DeveloperMDCM@users.noreply.github.com>
2023-01-20 20:56:20 -0500

    New

--- alibaba.js
+++ alibaba.js
@@ -1,4 +1,18 @@
+// ==UserScript==
+// @name         Alibaba download videos product
+// @namespace    https://github.com/DeveloperMDCM
+// @version      0.1
+// @description  Download videos from alibaba - Descarga el video de un producto
+// @author       MDCM
+// @match        *://*.alibaba.com/*
+// @icon         https://play-lh.googleusercontent.com/D09AsmYSxDpMWRSXbu54j2R_8sR-1OgbV2DXSI9_HIuY2IMZ8b8JgscWol6mikknaks
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    //https://github.com/DeveloperMDCM
+    // MDCM
 let video = document.querySelector('#main-video > div > div > div > video');
 const linkDiv = document.querySelector("#container > div.layout-content > div > div.screen-body > div.screen-layout > div.layout-left > div.main-layout > div.thumb-list > div > div > div.detail-next-slick-list");
 const link = document.createElement('DIV');
@@ -25,7 +39,7 @@ a:hover{
 <div style="margin: 6px 0;" class="container">
 <button style=" width: 100%;border-radius: 20px; " class="button-container">
   <a style="color: #fff;"  class="link-descarga" href="#">Download Video</a>
-</button> 
+</button>
 </div>
 `;
 
@@ -46,11 +60,11 @@ const download = async (url, filename) => {
 }
 
 
-link.addEventListener('click', ()  =>{
+link.addEventListener('click', ()=>{
   video = document.querySelector('#main-video > div > div > div > video');
   console.log(video.src)
   download(`${video.src}`,'video-MDCM');
-}) 
+})
 
 
 if(video) {
@@ -58,3 +72,6 @@ if(video) {
     video = document.querySelector('#main-video > div > div > div > video').src;
   },1000);
 };
+
+  
+})();
